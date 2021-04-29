a85846de0fab104718b540ee9aabe81602f67e4c
ardak <ardak.tileu@mediamonks.com>
2021-04-29 18:30:49 +0600

    feat: add package storybook

---
+++ packages/storybook/.storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    "../../brand-a/**/stories.@(ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@storybook/addon-actions",
+    "@storybook/addon-backgrounds",
+    "@storybook/addon-viewport",
+  ]
+}
